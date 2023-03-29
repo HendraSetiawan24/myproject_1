c00db77123f5ba7f616406395b090d408961f096
HendraSetiawan24 <setiawanh2490@gmail.com>
2023-03-29 17:25:23 +0700

    first commit

---
+++ Python training.py
@@ -0,0 +1,24 @@
+print("Hello World")
+
+# import sys
+# from termcolor import colored, cprint
+
+# text = colored("Hello World!", "yellow", attrs=["bold","concealed"])
+# print(text)
+# cprint("Hello World!", "blue", "on_yellow")
+
+# print_yellow_on_cyan = lambda x: cprint(x, "yellow", "on_cyan")
+# print_yellow_on_cyan("Hello, World!")
+# print_yellow_on_cyan("Hellom Universe!")
+
+import sys
+from termcolor import colored
+
+print(colored("Hello World", "red"))
+print(colored("Hello World", "blue"))
+print(colored("Hello World", "green"))
+print(colored("Hello World", "yellow"))
+print(colored("Hello World", "cyan"))
+
+
+
